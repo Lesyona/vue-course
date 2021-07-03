4d2407b36710d08a82c26d21cbe84c6332cfff55
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2021-07-04 00:12:33 +0300

    ДЗ к 3му уроку

--- lesson-03/src/App.vue
+++ lesson-03/src/App.vue
@@ -1,29 +1,69 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
+        <header>
+            <h1>My personal costs</h1>
+        </header>
 
-        <Calc />
+        <main>
+            <AddPayment @addNewPayment="addPaymentItem" />
+            <PaymentsDisplay :list="paymentsList"/>
+        </main>
     </div>
 </template>
 
 <script>
-import Calc from './components/Calc.vue';
+import PaymentsDisplay from './components/PaymentsDisplay.vue';
+import AddPayment from './components/AddPayment.vue';
 
 export default {
     name: 'App',
     components: {
-        Calc
+        PaymentsDisplay,
+        AddPayment
+    },
+    data: () => ({
+        paymentsList: [],
+    }),
+    methods: {
+        fetchData() {
+            return [
+                {
+                    date: "28.03.2020",
+                    category: "Food",
+                    value: 8734
+                },
+                {
+                    date: "12.04.2021",
+                    category: "Sport",
+                    value: 123
+                },
+                {
+                    date: "05.11.2020",
+                    category: "Internet",
+                    value: 490
+                },
+            ]
+        },
+        addPaymentItem(data) {
+            this.paymentsList.push(data);
+        },
+    },
+    created() {
+        this.paymentsList = this.fetchData();
     }
 }
 </script>
 
 <style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0 50px;
+    padding: 0;
+    font-family: Verdana, Arial, sans-serif;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    -webkit-font-smoothing: antialiased;
 }
 </style>
---
+++ lesson-03/src/components/PaymentsDisplay.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="payments-list">
+
+        <table class="payments__table">
+            <thead>
+                <tr class="payments__table__header-row">
+                    <th class="payments__table__header-cell">#</th>
+                    <th class="payments__table__header-cell">Date</th>
+                    <th class="payments__table__header-cell">Category</th>
+                    <th class="payments__table__header-cell">Value</th>
+                </tr>
+            </thead>
+
+            <tbody class="payments__table__content">
+                <tr
+                    v-for="(item, idx) in list"
+                    :key="idx"
+                    class="payments__table__row"
+                >
+                    <td class="payments__table__cell">{{ idx + 1 }}</td>
+                    <td class="payments__table__cell">{{ item.date }}</td>
+                    <td class="payments__table__cell">{{ item.category }}</td>
+                    <td class="payments__table__cell">{{ item.value }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PaymentsDisplay",
+    props: {
+        list: {
+            type: Array,
+            default: () => []
+        }
+    },
+    methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.payments-list {
+    max-width: 35%;
+}
+
+.payments__table {
+    min-width: 100%;
+    border: 0;
+    white-space: nowrap;
+    border-collapse: collapse;
+    table-layout: fixed;
+
+    &__row {
+        border-top: 1px solid rgba(0,0,0,.12);
+    }
+
+    &__cell,
+    &__header-cell {
+        padding-right: 16px;
+        padding-left: 16px;
+    }
+
+    &__header-cell {
+        font-weight: bold;
+        height: 55px;
+        text-align: left;
+        text-overflow: ellipsis;
+    }
+
+    &__cell {
+        height: 50px;
+    }
+}
+</style>
