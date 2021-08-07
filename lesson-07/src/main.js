dfec9a124f20fa5b0f23252723c79895dd0353dc
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2021-08-07 23:23:01 +0300

    ДЗ к 7му уроку

---
+++ lesson-07/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+import modal from './plugins/ModalEditPayment';
+
+Vue.config.productionTip = false
+Vue.use(modal);
+new Vue({
+  render: h => h(App),
+  store,
+  router,
+}).$mount('#app')
