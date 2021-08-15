6a804032ec592797432843625118c7e681800ab0
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2021-08-15 22:18:30 +0300

    ДЗ к 8му уроку

---
+++ lesson-08/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+      <header>
+          <h1>My personal costs</h1>
+      </header>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0 50px;
+    padding: 0;
+    font-family: Verdana, Arial, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+</style>
---
+++ lesson-08/src/components/Diagram.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="diagram">
+        <DoughnutChart
+            v-if="loaded"
+            :chart-data="chartData"
+            :options="options"
+        ></DoughnutChart>
+    </div>
+</template>
+
+<script>
+import DoughnutChart from "../assets/DoughnutChart.js";
+import {mapGetters} from "vuex";
+
+export default {
+    name: "Diagram",
+    components: {
+        DoughnutChart,
+    },
+    data(){
+        return {
+            loaded: false,
+            chartData: null,
+            options: {
+                responsive: true,
+                legend: {
+                    display: true,
+                    position: 'right',
+                },
+            }
+        }
+    },
+    computed: {
+        ...mapGetters({
+            chartDataState: 'getChartData',
+        }),
+        chartState() {
+            return this.chartDataState;
+        }
+    },
+    mounted() {
+        console.log('mounted');
+
+        this.chartData = {
+            labels: ['Red'],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [],
+                backgroundColor: [
+                    'rgb(252, 15, 102)',
+                    'rgb(15, 138, 252)',
+                    'rgb(248, 252, 15)',
+                    'rgb(43, 227, 104)',
+                    'rgb(224, 43, 227)'
+                ],
+            }]
+        }
+    },
+    watch: {
+        chartState() {
+            let i = 0;
+            for (let data in this.chartDataState) {
+                this.chartData.labels[i] = data;
+                this.chartData.datasets[0].data[i] = this.chartDataState[data];
+                i++;
+            }
+
+            this.loaded = true;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.diagram {
+    max-width: 300px;
+}
+</style>
+
+
