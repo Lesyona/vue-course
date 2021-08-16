9e720e6c5651c79da8611a3d4e466e95226f12ac
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2021-08-16 23:08:15 +0300

    ДЗ к 8му уроку

--- lesson-08/src/store/index.js
+++ lesson-08/src/store/index.js
@@ -53,17 +53,23 @@ export default new Vuex.Store({
             "page1": [
               {"id": 1, "date": "20.03.2020", "category": "Food", "value": 169},
               {"id": 2, "date": "21.03.2020", "category": "Navigation", "value": 50},
-              {"id": 3, "date": "22.03.2020", "category": "Sport", "value": 450}
-            ],
-            "page2": [
+              {"id": 3, "date": "22.03.2020", "category": "Sport", "value": 450},
               {"id": 4, "date": "23.03.2020", "category": "Entertainment", "value": 900},
               {"id": 5, "date": "24.03.2020", "category": "Education", "value": 1200},
               {"id": 6, "date": "25.03.2020", "category": "Food", "value": 200}
             ],
+            "page2": [
+              {"id": 7, "date": "23.03.2020", "category": "Entertainment", "value": 900},
+              {"id": 8, "date": "24.03.2020", "category": "Navigation", "value": 2000},
+              {"id": 9, "date": "25.03.2020", "category": "Food", "value": 200},
+              {"id": 10, "date": "22.03.2020", "category": "Sport", "value": 450},
+              {"id": 11, "date": "23.03.2020", "category": "Food", "value": 900},
+              {"id": 12, "date": "24.03.2020", "category": "Navigation", "value": 1200},
+            ],
             "page3": [
-              {"id": 7, "date": "23.03.2020", "category": "Entertainment", "value": 100},
-              {"id": 8, "date": "24.03.2020", "category": "Education", "value": 1500},
-              {"id": 9, "date": "25.03.2020", "category": "Food", "value": 200}
+              {"id": 13, "date": "23.03.2020", "category": "Entertainment", "value": 100},
+              {"id": 14, "date": "24.03.2020", "category": "Education", "value": 1500},
+              {"id": 15, "date": "25.03.2020", "category": "Food", "value": 200}
             ],
           };
           const pagesNumber = Object.keys(items).length;
